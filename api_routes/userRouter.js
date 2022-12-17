f7492e32e27191a68920d033bea8c3e50d20f43f
VikTheViking <48630150+VikTheViking@users.noreply.github.com>
2022-12-17 13:49:59 +0200

    added new user routes

--- api_routes/userRouter.js
+++ api_routes/userRouter.js
@@ -1,8 +1,11 @@
+const multer = require('multer');
 const router = require('express').Router();
 const User = require('../models/User');
 const Support = require('../models/SupMessage');
 const DTO = require('../models/DTO');
+const Bid = require('../models/Bid');
 const validate = require('../validation');
+const { bid } = require('../validation');
 
 const notAllowedMethod = (req, res) => {
     res.status(400).json({error: true, message: "Method not allowed!"});
@@ -22,7 +25,78 @@ router.route('/conversations')
 
 router.route('/bids')
 .get(async (req, res) => {
-    
+    if(req.user.type === "SME"){
+        try {
+            let bids = await Bid.find({from: req.user._id});
+            bids = bids.map(bid => bid.project);
+            const projects = await DTO.find({_id: {$in: bids}, status: "active"});
+            res.json({error: false, message: projects.map(prt => {
+                return {
+                    id: prt._id,
+                    name: prt.name,
+                    budget: prt.budget,
+                    duration: prt.duration,
+                    price: prt.price,
+                    date: prt.date
+                }
+            })});
+        } catch {
+            res.status(500).json({error: true, message: "Database operation error"});
+        }
+    } else if (req.user.type === "SPO") {
+        try {
+            const projects = await DTO.find({issuedBy: req.user._id, status: "active"});
+            res.json({error: false, message: projects.map(prt => {
+                return {
+                    id: prt._id,
+                    name: prt.name,
+                    budget: prt.budget,
+                    date: prt.date
+                }
+            })});
+        } catch {
+            res.status(500).json({error: true, message: "Database operation error"});
+        }
+    } else return res.status(400).json({error: true, message: "Unknown user type!"});
+})
+.all(notAllowedMethod);
+
+router.route('/bids/:project')
+.get(async (req, res) => {
+    try {
+        let bids = await Bid.find({project: req.params.project});
+        bids = bids.map(async (bid) => {
+            const user = await User.findOne({_id: bid.from});
+            return {
+                id: bid._id,
+                from: `${user.name} ${use.surname}`,
+                text: bid.text,
+                price: bid.price,
+                date: bid.date
+            }
+        })
+        res.json({error: false, message: bids});
+    } catch {
+        res.status(500).json({error: true, message: "Database operation error"});
+    }
+})
+.all(notAllowedMethod);
+
+router.route('/bid')
+.post(async (req, res) => {
+    // Only SPO can create projects
+    if (req.user.type !== "SME") return res.status(400).json({error: true, message: 'Only SME users can place bids'});
+    //Validation
+    let { error } = validate.bid.validate(req.body);
+    if (error) return res.status(400).json({error: true, message: error.details[0].message});
+    //Save to Database
+    try{
+        const bid = new Bid({...req.body, from: req.user._id});
+        await bid.save();
+        res.json({error: false, message: 'Project successfully created!'});
+    } catch(err) {
+        res.status(500).json({error: true, message: "Database operation error"});
+    }
 })
 .all(notAllowedMethod);
 
@@ -33,13 +107,18 @@ router.route('/finances')
 .all(notAllowedMethod);
 
 router.route('/private/info')
-.post(async (req, res) => {
+.post(multer().single('avatar'), async (req, res) => {
     //Validation
-    const { error } = validate.private.validate(req.body);
+    let { error } = validate.private.validate(req.body);
     if (error) return res.status(400).json({error: true, message: error.details[0].message});
+    if (req.file) {
+        error = validate.file.validate(req.body).error;
+        if (error) return res.status(400).json({error: true, message: error.details[0].message});
+    }
     //Update private info
     try{
-        await User.updateOne({_id: req.user._id}, {$set: req.body});
+        const data = !req.file ? req.body : {...req.body, file: req.file};
+        await User.updateOne({_id: req.user._id}, {$set: data});
         res.json({error: false, message: 'Private info successfully changed!'});
     } catch(err) {
         res.status(500).json({error: true, message: "Database operation error"});
@@ -50,20 +129,8 @@ router.route('/private/info')
 router.route('/private/avatar')
 .get(async (req, res) => {
     try{
-        const {avatar} = await User.findOne({_id: req.user._id});
+        const { avatar } = await User.findOne({_id: req.user._id});
         res.send(avatar);
-    } catch(err) {
-        res.status(500).json({error: true, message: "Database operation error"});
-    }
-})
-.post(async (req, res) => {
-    //Validation
-    const { error } = validate.avatar.validate(req.body);
-    if (error) return res.status(400).json({error: true, message: error.details[0].message});
-    //Save to Database
-    try{
-        await User.updateOne({_id: req.user._id}, {$set: {avatar: req.body}});
-        res.json({error: false, message: 'Avatar successfully changed!'});
     } catch(err) {
         res.status(500).json({error: true, message: "Database operation error"});
     }
@@ -74,14 +141,16 @@ router.route('/private/info/:id')
 .get(async (req, res) => {
     try{
         const user = await User.findOne({_id: req.params.id});
-        //get collection length for rank
         const members = await User.find({type: req.user.type});
         //get array of user dtos
+        let act = 0, fsh = 0, all = 0;
         if (user.DTO && user.DTO.length > 0) {
-            const userDTOs = await DTO.find({$and: user.DTO.map(dto => {
+            const userDTOs = await DTO.find({_id: {$in: user.DTO.map(dto => {
                 return {_id: dto};
-            })});
-            //await DTO.findById(user.DTO);
+            })}});
+            act = userDTOs.filter(dto => dto.status === "active").length;
+            fsh = userDTOs.filter(dto => dto.status === "finished").length;
+            all = userDTOs.length;
         };
         const userData = {
             type: user.type,
@@ -108,7 +177,7 @@ router.route('/private/info/:id')
 router.route('/private/avatar/:id')
 .get(async (req, res) => {
     try{
-        const {avatar} = await User.findOne({_id: req.params.id});
+        const { avatar } = await User.findOne({_id: req.params.id});
         res.send(avatar);
     } catch(err) {
         res.status(500).json({error: true, message: "Database operation error"});
@@ -117,20 +186,39 @@ router.route('/private/avatar/:id')
 .all(notAllowedMethod);
 
 router.route('/profile')
-.post(async (req, res) => {
+.post(multer().single('file'), async (req, res) => {
     if (req.user.type === "SPO") {
         //Validation
         const { error } = validate.profile.SPO.validate(req.body);
         if (error) return res.status(400).json({error: true, message: error.details[0].message});
         //Save data to DB
+        try{
+            await User.updateOne({_id: req.user._id}, {$set: req.body});
+            res.json({error: false, message: 'Profile info successfully changed!'});
+        } catch(err) {
+            res.status(500).json({error: true, message: "Database operation error"});
+        }
     } else if (req.user.type === "SME") {
         //Validation
-        const { error } = validate.profile.SME.validate(req.body);
+        let { error } = validate.profile.SME.validate(req.body);
         if (error) return res.status(400).json({error: true, message: error.details[0].message});
+        if (req.file) {
+            error = validate.file.validate(reg.file);
+            if (error) return res.status(400).json({error: true, message: error.details[0].message});
+        }
         //Save data to DB
+        try{
+            const data = !req.file ? req.body : {...req.body, file: req.file};
+            await User.updateOne({_id: req.user._id}, {$set: data});
+            res.json({error: false, message: 'Profile info successfully changed!'});
+        } catch(err) {
+            res.status(500).json({error: true, message: "Database operation error"});
+        }
     } else return res.status(400).json({error: true, message: "Unknown user type!"});
 })
 .all(notAllowedMethod);
+
+// ****All routes to interact with support messages****
 
 router.route('/support')
 .post(async (req, res) => {
@@ -161,6 +249,82 @@ router.route('/support/:id')
     try{
         await Support.deleteOne({_id: req.params.id});
         res.json({error: false, message: 'Message to support successfully deleted!'});
+    } catch(err) {
+        res.status(500).json({error: true, message: "Database operation error"});
+    }
+})
+.all(notAllowedMethod);
+
+// ****All routes for interection with projects****
+
+router.route('/project')
+.post(multer().single('file'), async (req, res) => {
+    // Only SPO can create projects
+    if (req.user.type !== "SPO") return res.status(400).json({error: true, message: 'Only SPO users can create projects'});
+    //Validation
+    let { error } = validate.project.validate(req.body);
+    if (error) return res.status(400).json({error: true, message: error.details[0].message});
+    if (req.file) {
+        error = validate.file.validate(reg.file);
+        if (error) return res.status(400).json({error: true, message: error.details[0].message});
+    }
+    //Save to Database
+    try{
+        const data = !req.file ? req.body : {...req.body, file: req.file};
+        const project = new DTO(data);
+        project.save();
+        res.json({error: false, message: 'Project successfully created!'});
+    } catch(err) {
+        res.status(500).json({error: true, message: "Database operation error"});
+    }
+})
+.all(notAllowedMethod);
+
+router.route('/project/:id')
+.get(async (req, res) => {
+    const data = await Promise.all([
+        DTO.findOne({_id: req.params.id}),
+        User.findOne({_id: dto.issuedBy}),
+        Bid.find({project: req.params.id})
+        .then(bids => {
+            return bids.map(async (bid) => {
+                const user = await User.findOne({_id: bid.from});
+                return {
+                    id: bid._id,
+                    from: `${user.name} ${use.surname}`,
+                    text: bid.text,
+                    price: bid.price,
+                    date: bid.date
+                }
+            })
+        })
+    ])
+    .then(data => {
+        const dto = data[0], issuer = data[1], bids = data[2];
+
+        return {
+            name: dto.name,
+            category: dto.category,
+            about: dto.about,
+            issuedBy: `${issuer.name} ${issuer.surname}`,
+            bids: bids,
+            date: dto.date,
+            until: dto.until,
+            views: dto.views,
+            budget: dto.budget
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: true, err: err, message: "Database operation error"});
+    });
+
+    res.json({error: false, result: data});
+})
+.delete(async (req, res) => {
+    try{
+        const dto = await DTO.deleteOne({_id: req.params.id});
+        if (req.user._id !== dto.issuedBy) return res.status(400).json({error: true, message: "Only the onwer of project can remove it"});
+        res.json({error: false, message: 'Poject successfully deleted!'});
     } catch(err) {
         res.status(500).json({error: true, message: "Database operation error"});
     }
